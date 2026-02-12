b338613cad1c4caa67e88e2884635964668b2028
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-02-12 18:27:07 +0700

    feat: update README.md

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # go minitask
 
+## First Minitask
+
 this repo is minitask to practice go
 
 * task 1
@@ -15,4 +17,10 @@ Task 2 complete using struct hello
 * task 5
 Task 5 complete using struct first and calculate variable num
 
-<!--  -->
+## Second Minitask
+
+in the second task i copy the initial value `scores := []int{50, 75, 66, 20, 32, 90}`
+
+from both value I take each items and in the first I add num 88
+
+![test](./minitask2.mp4)
--- main.go
+++ main.go
@@ -6,87 +6,30 @@ import (
 )
 
 func main() {
-	// Slice()
-	// var container []int
+	// lib.Slice()
+	// initial value
 	scores := []int{50, 75, 66, 20, 32, 90}
-	fmt.Println(scores)
+	// fmt.Println(scores)
+	// make empty sliceusing make
 	container := make([]int, len(scores))
-	copy(container, scores)
 	// fmt.Println(container)
-
+	// fmt.Println("container")
+	// copy value from scores and append into container
+	copy(container, scores)
+	// take value scores until 66
 	i := slices.Index(scores, 66)
-	// fmt.Println(i)
 	rawI := scores[:i+1]
-
-	// fmt.Println(append(rawI, 88))
 	result := append(rawI, 88)
-	// fmt.Println(rawI)
-
-	// fmt.Println("oihdoshfoehf")
-
+	// take value container start with 20
 	j := slices.Index(container, 20)
-	// fmt.Println(j)
 	rawJ := container[j:]
-	// fmt.Println(rawJ)
 
 	var finalResult []int = append(result, rawJ...)
 
-	fmt.Println(finalResult)
-
 	for v := range finalResult {
+		// looping process
 		fmt.Println(finalResult[v])
 	}
-
-	// fmt.Println(scores)
-	// fmt.Println("test")
-	// fmt.Println(i)
-	// fmt.Println(scores[i])
-	// numbers := []int{1, 2, 3}
-	// for v := range scores {
-	// 	fmt.Println(scores[v])
-	// 	append(container, scores)
-	// }
-	// test := slices.Contains(scores, 66)
-	// i := slices.Index(scores, 66)
-	// j := slices.Index(scores, 66)
-	// fmt.Println("j")
-	// fmt.Println(j)
-	// rawI := scores[:i+1]
-	// // rawJ := scores[i+1:]
-	// var rawJ []int
-	// rawJ = scores[j+1:]
-	// fmt.Println("rawJ")
-	// fmt.Println(rawJ)
-
-	// // fmt.Println(append(scores, 8))
-
-	// // fmt.Println(i)
-	// // fmt.Println(j)
-	// // fmt.Println(test)
-	// // fmt.Println(scores[2])
-	// // fmt.Println("rawI")
-	// // fmt.Println(rawI)
-	// // fmt.Println("rawJ")
-	// // fmt.Println(rawJ)
-	// fmt.Println("didieu yeuh")
-	// fmt.Println(append(rawI, 88))
-	// fmt.Println(rawJ)
-	// // result := append(rawI, rawJ...)
-	// // fmt.Println(result)
-	// fmt.Println("result")
-	// fmt.Println(append(rawI, rawJ...))
-
-	// for _, v := range scores {
-	// 	container = append(container, v)
-	// }
-
-	// fmt.Println(container)
-
-	// numbers := []int{0, 42, 8}
-	// fmt.Println(slices.Equal(numbers, []int{0, 42, 8}))
-	// fmt.Println(slices.Equal(numbers, []int{10}))
-
-	// numbers := []int{0, 1, 2, 3}
-	// fmt.Println(slices.Contains(numbers, 2))
-	// fmt.Println(slices.Contains(numbers, 4))
+	// final result
+	fmt.Println(finalResult)
 }
